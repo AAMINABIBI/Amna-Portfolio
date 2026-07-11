fe835fd1ceed7a25aa39787f5480d67d86c34b6b
AAMINABIBI <aaminabibi2324@gmail.com>
2026-07-11 07:51:50 +0500

    Full restyle to cyan/violet theme

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import project1 from '../assets/images/mock01.png';
-import project2 from '../assets/images/E-commProj.png';
-import project3 from '../assets/images/Freelancers_portfolio.png';
-import project4 from '../assets/images/mock04.png';
-import project5 from '../assets/images/mock05.png';
-import project6 from '../assets/images/mock06.png';
+import visioVoxImg from '../assets/images/mock03.png';
+import ecommImg from '../assets/images/E-commProj.png';
+import freelancerImg from '../assets/images/Freelancers_portfolio.png';
+import calculatorImg from '../assets/images/calculator.png';
+import easyPaisaImg from '../assets/images/mock02.png';
+import todoImg from '../assets/images/mock01.png';
+import codeCraftImg from '../assets/images/mock06.png';
+import shinepointImg from '../assets/images/mock07.png';
+import fitnessAdminImg from '../assets/images/mock05.png';
 import { INFO } from '../data/user';
 import '../assets/styles/Project.scss';
 import { Button } from '@mui/material';
 
-const projectImages = [project1, project2, project3, project4, project5, project6];
+// Order must match the order of INFO.projects in data/user.ts
+const projectImages = [
+  visioVoxImg,
+  ecommImg,
+  freelancerImg,
+  calculatorImg,
+  easyPaisaImg,
+  todoImg,
+  codeCraftImg,
+  shinepointImg,
+  fitnessAdminImg,
+];
 
 function Project() {
   return (
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -7,6 +7,13 @@ import 'react-vertical-timeline-component/style.min.css';
 import { INFO } from '../data/user';
 import '../assets/styles/Timeline.scss';
 
+const cardStyle = {
+  background: '#111827', // matches --bg-panel from index.scss
+  color: '#f3f6fb', // matches --text-hi
+};
+const arrowStyle = { borderRight: '7px solid #111827' };
+const iconStyle = { background: '#5eead4', color: '#04120f' };
+
 function Timeline() {
   return (
     <div id="history">
@@ -17,10 +24,10 @@ function Timeline() {
             <VerticalTimelineElement
               key={index}
               className="vertical-timeline-element--work"
-              contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
-              contentArrowStyle={{ borderRight: '7px solid white' }}
+              contentStyle={cardStyle}
+              contentArrowStyle={arrowStyle}
               date={exp.period}
-              iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+              iconStyle={iconStyle}
               icon={<FontAwesomeIcon icon={faBriefcase} />}
             >
               <h3 className="vertical-timeline-element-title">{exp.role}</h3>
@@ -31,22 +38,16 @@ function Timeline() {
             <VerticalTimelineElement
               key={index}
               className="education"
-              contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
-              contentArrowStyle={{ borderRight: '7px solid white' }}
+              contentStyle={cardStyle}
+              contentArrowStyle={arrowStyle}
               date={edu.period}
-              iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+              iconStyle={iconStyle}
               icon={<FontAwesomeIcon icon={faBriefcase} />}
             >
               <h3 className="element-title">{edu.degree}</h3>
               <h4 className="element-subtitle">{edu.institution}</h4>
             </VerticalTimelineElement>
           ))}
-          
-           
-
-          
-
-          
         </VerticalTimeline>
       </div>
     </div>
